ea66f71b863bce068d3d92f2d7670ff3cd12524b
Empoweredbrush <Empoweredbrushfour@gmail.com>
2026-06-29 09:35:35 -0600

    Changed the variables for the links to be able to just access the pdf and not download it.

--- fill_json.py
+++ fill_json.py
@@ -1,2 +1,33 @@
+import json
 import fitz
+import requests
+
+
+Factions = [
+    {
+    "name": "Axis",
+    "url": "https://warlord-community.warlordgames.com/wp-content/uploads/2026/03/K47_Army-List_Axis_MAR2026.pdf",
+    "output": "k47ArmyData.json"
+    },
+    {
+        "name": "British",
+        "url": "https://warlord-community.warlordgames.com/wp-content/uploads/2026/03/K47_Army-List_British-Commonwealth_MAR2026.pdf",
+        "output": "k47ArmyData.json"
+    },
+    {
+        "name": "Japan",
+        "url": "https://warlord-community.warlordgames.com/wp-content/uploads/2026/03/K47_Army-List_Japan_MAR2026.pdf",
+        "output": "k47ArmyData.json"
+    },
+    {
+        "name": "USA",
+        "url": "https://warlord-community.warlordgames.com/wp-content/uploads/2026/03/K47_Army-List_USA_240326.pdf",
+        "output": "k47ArmyData.json"
+    },
+    {
+        "name": "Soviet",
+        "url": "https://warlord-community.warlordgames.com/wp-content/uploads/2026/03/K47_Army-List_Soviet_MAR2026.pdf",
+        "output": "k47ArmyData.json"
+    }
+]
 
